349830bf1f2ac8ef7ed2796c86b686cb8f8cc841
A.R. Bagas Danang Haditio <arbagasdanangh@gmail.com>
2026-09-10 13:29:41 +0700

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,16 +1,8 @@
-/*import mysql from 'mysql2/promise';
-
-export const db = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'ladpm'
-});*/
-// src/lib/db.js
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
   host: '103.93.133.133',
+  port: 3306,
   user: 'appuser',
   password: 'aATGBIdRtS50BbunLZuINgfT',
   database: 'appdb',
@@ -19,3 +11,10 @@ export const db = mysql.createPool({
   queueLimit: 0,
 });
 
+try {
+  const connection = await db.getConnection();
+  console.log('MySQL connected!');
+  connection.release();
+} catch (error) {
+  console.error('MySQL connection error:', error);
+}
